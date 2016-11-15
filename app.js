36683b76cfb893f8fe237c2a5c31e4b8869edde0
Chana Reynolds <chana777@gmail.com>
2016-11-15 11:36:21 -0700

    click-function

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     var sizeObj = {};
     $('select.topSize').change(function() {
         sizeObj.topSize = $(this).val();
-        console.log(sizeObj);
+
 
     });
     $('select.bottomSize').change(function() {
         sizeObj.bottomSize = $(this).val();
-        console.log(sizeObj);
+
 
     });
     $('select.price').change(function() {
@@ -17,18 +17,24 @@ $(document).ready(function() {
 
     });
 
-    $('button').click(function() {
-
-    });
 
     var key = "uid2496-36840364-7"
     var search = ["womens-tops", "pants", "shoes", "bags"]
     var url = "http://api.shopstyle.com/api/v2/products?pid="
-    $.get(url + key + "&fts=" + search[0] + "&offset=0&limit=10", function(productList) {
-        var id = productList.products[0].id
-        $.get("http://api.shopstyle.com/api/v2/products/" + id + "?pid=uid2496-36840364-7", function(product) {
-            console.log(product);
-        })
+
+    $('button').click(function() {
+
+        var sizeObj = $('select.topSize').val();
+        $('select.bottomSize').val()
+        $('select.price').val()
+
+        $.get(url + key + "&fts=" + search[0] + "&offset=0&limit=10", function(productList) {
+            var id = productList.products[0].id
+            $.get("http://api.shopstyle.com/api/v2/products/" + id + "?pid=uid2496-36840364-7", function(product) {
+                console.log(product);
+            })
+        });
+
     });
 
 });
