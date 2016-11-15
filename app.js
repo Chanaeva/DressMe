7cdc4909f57bf51123a14eea4783723e2898002e
Chana Reynolds <chana777@gmail.com>
2016-11-15 08:22:25 -0700

    Button+addedAPIcall

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ $(document).ready(function() {
 
     });
 
-    var size = $('size').val()
+    $('button').click(function() {
+
+    });
+
+    var key = "uid2496-36840364-7"
+    var search = ["womens-tops", "pants", "shoes", "bags"]
+    var url = "http://api.shopstyle.com/api/v2/products?pid="
+    $.get(url + key + "&fts=" + search[0] + "&offset=0&limit=10", function(productList) {
+        var id = productList.products[0].id
+        $.get("http://api.shopstyle.com/api/v2/products/" + id + "?pid=uid2496-36840364-7", function(product) {
+            console.log(product);
+        })
+    });
 
 });
