835c9762ef2e9983b8dc9d10704a607c231f95d7
Chana Reynolds <chana777@gmail.com>
2016-11-16 13:38:38 -0700

    Getting-Back-Object

--- app.js
+++ app.js
@@ -2,35 +2,33 @@
 
      $('select').material_select();
 
-     var catObj = {};
-
+     var inputInfo = [];
 
      var key = "uid2496-36840364-7";
      var url = "http://api.shopstyle.com/api/v2/products?pid=";
 
      $('button').click(function() {
-          catObj.topCat = $('.topCat').val();
-          catObj.bottomCat = $('.bottomCat').val();
-          catObj.shoeCat = $('.shoeCat').val();
-          catObj.price = $('price').val();
-
-         console.log(catObj);
+         inputInfo.push($('.topCat').val())
+         inputInfo.push($('.bottomCat').val())
+         inputInfo.push($('.shoeCat').val())
+             //  inputInfo.push($('price').val())
 
 
-         // after the button is clicked the values from the above cats will go into this get function//
+         var length = 4;
+
+         for (var i = 0; i < length; i++) {
+             $.get(url + key + "&fts=" + inputInfo[i] + "&offset=0&limit=10", function(productList) {
+                 console.log(productList);
 
 
-         $.get(url + key + "&fts=" + catObj.topCat + "&fts=" + catObj.bottomCat + "&fts=" + catObj.shoeCat + "&fts=" + catObj.price + "&offset=0&limit=10", function(productList) {
-             console.log(productList);
-             //  then I need a new object that provides me with the img for each item in the cat values//
-             // then I need to display the images onto my page.
+                 var id = productList.products[0].id
+                 $.get("http://api.shopstyle.com/api/v2/products/" + id + "?pid=uid2496-36840364-7", function(product) {
+                     console.log(product);
 
-              var id = productList.products[0].id
-              $.get("http://api.shopstyle.com/api/v2/products/" + id + "?pid=uid2496-36840364-7", function(product) {
-                  console.log(product);
+                 })
+             });
 
-              })
-         });
+         }
 
      });
 
